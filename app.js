3a4b2a1fe553237018df7d8c368905b06fb92043
kulakowka <kulakowka@gmail.com>
2016-02-07 04:39:44 +0300

    add pmx

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const logger = require('morgan')
 const bodyParser = require('body-parser')
 
+var pmx = require('pmx').init({
+  custom_probes : true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
+  network       : true, // Network monitoring at the application level
+  ports         : true  // Shows which ports your app is listening on (default: false)
+})
+
 var app = express()
 
 app.use(logger('dev'))
